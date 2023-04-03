d8b2e576ee406b6776dc6baf9fa594cc57aab234
W0474997SteveArmstrong <113034949+W0474997SteveArmstrong@users.noreply.github.com>
2023-04-03 14:21:46 -0300

    First Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+ //main application.
+ //Every single component is contained within this App component
+
+ //rafce shorthand to create a react functional component named App
+
+ import React from 'react'
+ 
+ const App = () => {
+   return (
+     <div>App</div>
+   )
+ }
+ 
+ export default App
