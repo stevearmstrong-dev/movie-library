81c8476739cf8574f515f63d86cb9b8ca9f06997
W0474997SteveArmstrong <113034949+W0474997SteveArmstrong@users.noreply.github.com>
2023-04-04 16:24:15 -0300

    Added Route and Routes as per React v6.0 instead of switch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,22 @@
- //main application.
- //Every single component is contained within this App component
+// main application.
+// Every single component is contained within this App component
+// rafce shorthand to create a react functional component named App
 
- //rafce shorthand to create a react functional component named App
+import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Route, Routes } from 'react-router-dom';
 
- import React from 'react'
- 
- const App = () => {
-   return (
-     <div>App</div>
-   )
- }
- 
- export default App
+const App = () => (
+  <div>
+    <CssBaseline />
+    <main>
+      <Routes>
+        <Route path="/movie/:id" element={<h1>Movie Information</h1>} />
+        <Route path="/actors/:id" element={<h1>Actors</h1>} />
+        <Route path="/" element={<h1>Movies</h1>} />
+        <Route path="/profile/:id" element={<h1>Profile</h1>} />
+      </Routes>
+    </main>
+  </div>
+);
+export default App;
