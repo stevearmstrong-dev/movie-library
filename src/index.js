e88b4e2d67d8bcc136ec4d6d5dd03f4e24a9d234
W0474997SteveArmstrong <113034949+W0474997SteveArmstrong@users.noreply.github.com>
2023-04-04 16:25:36 -0300

    Added browserrouter to the app component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
-//connects our react application to the index.html file found in the public folder
-import ReactDOM from 'react-dom';
+// connects our react application to the index.html file found in the public folder
+import { createRoot } from 'react-dom/client';
+// import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
