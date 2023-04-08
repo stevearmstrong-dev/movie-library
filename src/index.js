8b77a167f71544ced7b63baee766039d3f1131ec
W0474997SteveArmstrong <113034949+W0474997SteveArmstrong@users.noreply.github.com>
2023-04-08 13:14:35 -0300

    Added ThemeProvider from material UI

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 // import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import App from './components/App';
+
+const theme = createTheme({});
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
 );
