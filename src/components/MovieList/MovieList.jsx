a86d0f15a4bab3b98aac46b43caef30f3ed36c87
W0474997SteveArmstrong <113034949+W0474997SteveArmstrong@users.noreply.github.com>
2023-04-10 17:44:43 -0300

    Created MovieList component with key as index and value as movie

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+
+import { StyledMoviesContainer } from './styles';
+import Movie from '../Movie/Movie';
+
+const MovieList = ({ movies }) => {
+  console.log('Movie List');
+  return (
+    <StyledMoviesContainer>
+      {movies.results.map((movie, index) => (
+        <Movie key={index} movie={movie} i={index} />
+      ))}
+    </StyledMoviesContainer>
+  );
+};
+
+export default MovieList;
