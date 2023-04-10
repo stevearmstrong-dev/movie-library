87c299ad69e52ebc031f6c814dddc1a0cfed314b
W0474997SteveArmstrong <113034949+W0474997SteveArmstrong@users.noreply.github.com>
2023-04-10 08:51:18 -0300

    Created API using tmdbApiKey and mentioned the endpoints, reducerPath and query

---
+++ src/services/TMDB.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const tmdbApiKey = process.env.TMDB_KEY;
+const page = 1;
+// https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
+export const tmdbApi = createApi({
+  reducerPath: 'tmdbApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
+  endpoints: (builder) => ({
+    // Get Movies by Type
+    getMovies: builder.query({
+      query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
+    }),
+  }),
+});
+// redux-toolkit query will automatically create this hook for us and it automatically calls the getMovies api endpoint for us
+export const {
+  useGetMoviesQuery,
+} = tmdbApi;
