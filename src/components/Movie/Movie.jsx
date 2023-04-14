ff3fa9546d00733abee15f58a93558f56c9f032d
W0474997SteveArmstrong <113034949+W0474997SteveArmstrong@users.noreply.github.com>
2023-04-14 11:50:10 -0300

    Enhance Movie component with image, tooltip, rating, and animation

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { Typography, Grow, Tooltip, Rating } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { StyledMovie, Title } from './styles';
+import { StyledImages, StyledLinks, StyledMovie, Title } from './styles';
 
 const Movie = ({ movie, index }) => {
-  console.log(movie, index);
+  console.log('index is', movie, index);
   return (
     <StyledMovie item xs={12} sm={6} md={4} lg={3} xl={2}>
-      <Title variant="h5">
-        {movie.title}
-      </Title>
+      <Grow in key={index} timeout={(index + 1) * 250}>
+        <StyledLinks to={`/movie/${movie.id}`}>
+          <StyledImages alt={movie.title} src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : 'https://www.fillmurray.com/200/300'} />
+          <Title variant="h5">
+            {movie.title}
+          </Title>
+          <Tooltip disableTouchListener title={`${movie.vote_average}/10`}>
+            <div>
+              <Rating readOnly value={movie.vote_average / 2} precision={0.1} />
+            </div>
+          </Tooltip>
+        </StyledLinks>
+      </Grow>
     </StyledMovie>
   );
 };
