6fa0aff51436f3b57a0ee91a14e06a8c7c4aa70a
W0474997SteveArmstrong <113034949+W0474997SteveArmstrong@users.noreply.github.com>
2023-04-14 11:46:59 -0300

    Added styles for movie images and movie title links

--- src/components/Movie/styles.js
+++ src/components/Movie/styles.js
@@ -1,14 +1,15 @@
 import { styled } from '@mui/system';
-import { Grid, Typography } from '@mui/material';
+import { Grid, Link, Typography } from '@mui/material';
 
 export const StyledDiv = styled('div')({
   display: 'flex',
   height: '100%',
 });
 
-export const StyledMovie = styled(Grid)(({ theme }) => ({
+export const StyledMovie = styled(Grid)({
   padding: '10px',
-}));
+  display: 'flex',
+});
 
 export const Title = styled(Typography)(({ theme }) => ({
   color: theme.palette.text.primary,
@@ -19,4 +20,29 @@ export const Title = styled(Typography)(({ theme }) => ({
   marginTop: '10px',
   marginBottom: 0,
   textAlign: 'center',
+}));
+
+export const StyledLinks = styled(Link)(({ theme }) => ({
+  // color: theme.palette.text.primary,
+  // textDecoration: 'none',
+  alignItems: 'center',
+  fontWeight: 'bolder',
+  textDecoration: 'none',
+  [theme.breakpoints.up('xs')]: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  '&:hover': {
+    cursor: 'pointer',
+  },
+}));
+
+export const StyledImages = styled('img')(({ theme }) => ({
+  //   width: '70%',
+  borderRadius: '20px',
+  height: '300px',
+  marginBottom: '10px',
+  '&:hover': {
+    transform: 'scale(1.05)',
+  },
 }));
