a5a83e3c594850d5bc0a0a89f03f9229c9963365
W0474997SteveArmstrong <113034949+W0474997SteveArmstrong@users.noreply.github.com>
2023-04-14 21:54:59 -0300

    Added custom made reducer based on Genre and Category

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { tmdbApi } from '../services/TMDB';
+import genreOrCategoryReducer from '../features/currentGenreOrCategory';
 
 export default configureStore({
   reducer: {
+    // reducer made by redux toolkit query
     [tmdbApi.reducerPath]: tmdbApi.reducer,
+    // custom made reducer
+    currentGenreOrCategory: genreOrCategoryReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tmdbApi.middleware),
 });
