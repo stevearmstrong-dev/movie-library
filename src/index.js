ffb4e9d60e717caccd356ebf86eca59626371ee7
W0474997SteveArmstrong <113034949+W0474997SteveArmstrong@users.noreply.github.com>
2023-04-16 10:04:27 -0300

    Integrate ToggleColorModeProvider for light/dark theme switching in index

--- src/index.js
+++ src/index.js
@@ -3,22 +3,20 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 // import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Provider } from 'react-redux';
 import store from './app/store';
 import App from './components/App';
-
-const theme = createTheme({});
+import ToggleColorModeProvider from './utils/ToggleColorMode';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
+    <ToggleColorModeProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ThemeProvider>
+    </ToggleColorModeProvider>
   </Provider>,
 );
